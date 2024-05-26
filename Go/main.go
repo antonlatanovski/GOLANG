46c8fed702e2f228128b5841f42ee0d951727f55
antonlatanovski <153992260+antonlatanovski@users.noreply.github.com>
2024-05-26 15:02:10 +0300

    Add files via upload

---
+++ Go/main.go
@@ -0,0 +1,83 @@
+package main
+
+import "fmt"
+
+func main() {
+// 	arr:=[]int{3,4,1,9,5,2,7}
+// 	bubblSortArr := SortArr(arr)
+// fmt.Printf("sortArr%#v\n",bubblSortArr)	
+// slise:=arr[0:6]
+// bubblSortSlise := sortSlise(slise)
+// fmt.Printf("sortSlise%#v\n",bubblSortSlise)
+// NewSlise:=append(slise, 2)
+// fmt.Printf("%#v\n",NewSlise)
+// 	}
+// func SortArr(arr[]int)[]int{
+// 	for i:=0;i<len(arr)-1;i++{
+// 		for j:=0;j<len(arr)-1;j++{
+// 			if arr[j]>arr[j+1]{
+// 				arr[j],arr[j+1] =arr[j+1],arr[j]
+// 			}
+// 		}
+// 	}
+// 	return arr
+// }
+// func sortSlise(slise[]int)[]int{
+// 	for i:=0;i<len(slise)-1;i++{
+// 		for j:=0;j<len(slise)-1;j++{
+// 			if slise[j]>slise[j+1]{
+// 				slise[j],slise[j+1] =slise[j+1],slise[j]
+// 			}
+// 		}
+// 	}
+// 	return slise
+// selectionArr := SelectionSort(arr)
+// fmt.Printf("Arr%#v\n",selectionArr)
+//  }
+//  func SelectionSort(arr[]int)[]int{
+// 	for i:=0;i<len(arr)-1;i++ {
+// 		minindex :=i
+// 		for j:=i+1; j<len(arr);j++{
+// 		if 	arr[minindex]>arr[j]{
+// 			minindex = j
+// 		}
+// 		}
+// 		if minindex!=i {
+// 			arr[minindex],arr[i]=arr[i],arr[minindex]
+// 		}
+// 	}
+// 	return arr
+// InsertArr := InsertionArrSort(arr)
+// fmt.Printf("Arr%#v\n",InsertArr)
+//  }
+//  func InsertionArrSort(arr[]int)[]int{
+// 	for i:=1;i<len(arr);i++{
+// 		sorted := i-1
+// 		for sorted!=-1 && arr[sorted]>arr[sorted+1]{
+// 			arr[sorted],arr[sorted+1]=arr[sorted+1],arr[sorted]
+// 			sorted--
+// 		}
+// 	}
+// 	return arr
+//  }
+
+//  func main(){
+// 	a:=[]int{1,2,3}
+// 	b:=[]int{1,2,3}
+// 	fmt.Println(equil(a,b))
+//   }
+// func equil(a,b[]int)bool{
+// if len(a)!=len(b){
+// 	return false
+// }
+// for i,v:= range a{
+// 	if v!= b[i]{
+// 		return false
+// 	}
+// }
+// return true
+a:=[]int{37,5,1,2}
+b:=[]int{6,2,4,37} 
+
+
+}
